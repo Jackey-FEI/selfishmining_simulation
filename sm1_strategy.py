d6444fb67ad083ed0c087d3ad8c637fc09511ff3
jackeyf <yangfei@umich.edu>
2024-04-22 19:27:28 -0400

    Finish a naive implementation of selfishmining

--- sm1_strategy.py
+++ sm1_strategy.py
@@ -11,9 +11,16 @@ sys.path.insert(0, os.path.abspath(
 
 class SelfishMining:
     def __init__(self, show_log=False):
+        random.seed(None)
         self._alpha = 0
         self._gamma = 0
         self.selfish_mining_revenue = 0
+        self.public_chain_length = 0
+        self.private_chain_length = 0
+        self.selfish_mining_block = 0
+        self.honest_mining_block = 0
+        self.selfish_naive_mining_block = 0 # caculate the block number if seflish miner honestly mining
+        self.delta = 0 # same as state delta which is the difference between the private and public chain length
 
     @property
     def alpha(self):
@@ -37,14 +44,6 @@ class SelfishMining:
         self._gamma = value
         return
 
-    @property
-    def revenue(self):
-        return self.__selfish_miner_revenue
-
-    @property
-    def stale_block(self):
-        return self.__total_stale_block
-
     def print_input_statistic(self):
         print('alpha is : {}'.format(self._alpha))
         print('gamma is : {}'.format(self._gamma))
@@ -55,28 +54,70 @@ class SelfishMining:
         # TODO: Implement the simulation, and calculate the revenue after the simulation
         # When the random number is less than alpha, 
         # the selfish miner will mine a block, otherwise the honest miner will mine a block
+        self.iteration = iteration
+        for i in range(iteration):
+            rand = random.uniform(0, 1)
+            if rand <= self._alpha:
+                self.selfish_mining()
+                self.selfish_naive_mining_block += 1
+            else:
+                self.honest_mining()
+        if self.private_chain_length >=2 :
+            self.selfish_mining_block += self.private_chain_length
 
+        self.calculating_revenue()
+        self.print_final_result()
         return
 
     def selfish_mining(self):
         # mine a block and increase the private chain length
         # may need to update the public chain length
-
+        self.delta = self.private_chain_length - self.public_chain_length
+        self.private_chain_length += 1
+        if self.delta == 0 and self.private_chain_length == 2: # TODO what if private chain leangth > 2, should we adopt this strategy
+            self.selfish_mining_block += self.private_chain_length 
+            self.private_chain_length = 0
+            self.public_chain_length = 0
         return
 
     def honest_mining(self):
         # mine a block and increase the public chain length
+        # disclose the private chain if the delta is 0
+        self.delta = self.private_chain_length - self.public_chain_length
+        self.public_chain_length += 1
+        if self.delta == 0 and self.private_chain_length > 0:
+            rand = random.uniform(0, 1)
+            if rand <= self._gamma: # mining on the private pool honest miner won
+                self.honest_mining_block+=1
+                self.selfish_mining_block+=1
+            else:                   # mining on the public pool honest miner won
+                self.honest_mining_block+=2
+            self.private_chain_length = 0
+            self.public_chain_length = 0
+        elif self.delta == 2:
+            # selfish miner disclose the chain and get all the profit rightnow
+            self.selfish_mining_block += self.private_chain_length 
+            self.private_chain_length = 0
+            self.public_chain_length = 0
+        elif self.delta == 0:
+            self.honest_mining_block += 1
+            self.private_chain_length = 0
+            self.public_chain_length = 0
 
         return
 
     def calculating_revenue(self):
         # calculate the revenue of the selfish miner and the honest miner
-
+        self.total_mining_block  = self.honest_mining_block + self.selfish_mining_block
+        self.wasted_iteration = self.iteration - self.total_mining_block
+        self.selfish_mining_revenue = float(self.selfish_mining_block) / self.total_mining_block
+        self.selfish_honest_mining_revenue = float(self.selfish_naive_mining_block) / self.iteration
         return
     
     def print_final_result(self):
+        print('Total mining block is : {}'.format(self.total_mining_block))
         print('Selfish Mining Revenue is : {}'.format(self.selfish_mining_revenue))
-        print("Honest Mining Revenue is : {}".format(1 - self.selfish_mining_revenue))
+        print('Selfish Honest Mining Revenue is : {}'.format(self.selfish_honest_mining_revenue))
 
         return
 
